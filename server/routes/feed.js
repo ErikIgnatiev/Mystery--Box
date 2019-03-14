e7238157b49bca605925efaee4c6800402088155
ErikIgnatiev <erik.ignatiyev@gmail.com>
2019-03-14 08:44:42 +0200

    server change

--- server/routes/feed.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const feedController = require('../controllers/feed');
-const isAuth = require('../middleware/is-auth');
-
-router.get('/orders', feedController.getOrders);
-router.post('/create', feedController.createOrder);
-
-module.exports = router;
